75391639ba9dfffc24f7f57873fadefdcba50d36
kelbefaast <55721275+kelbefaast@users.noreply.github.com>
2020-10-15 02:00:43 -0700

    fixed old pages added python and p5js and codehs

---
+++ scripts/p5_2_brick-wall.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(800, 800);
+    }
+function draw() {
+    if (mouseIsPressed) {
+        rect(mouseX, mouseY, 80, 80);
+        fill(255, 0, 0)
+        
+    }
+    
+    }
+    
